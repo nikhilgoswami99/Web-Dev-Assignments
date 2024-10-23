1dab11102395a371bbccd8a3eca2e94d9cfb75b4
Nikhil Goswami <nikhil.goswami.18121999@gmail.com>
2024-10-23 23:03:11 +0530

    adding DOM project

---
+++ DOM_Manipulation/index.js
@@ -0,0 +1,13 @@
+const div = document.getElementById("container");
+
+div.style.backgroundColor = "rgb(255, 255, 0)";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
+div.style.color = "blue";
+
+
+
